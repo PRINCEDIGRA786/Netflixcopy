7f4ba85d89e88021419c93e0a8e4601805613964
Prince <121872553+PRINCEDIGRA786@users.noreply.github.com>
2024-01-11 22:51:12 +0530

    Update Moviestate.js

--- frontend/src/contextapi/Moviestate.js
+++ frontend/src/contextapi/Moviestate.js
@@ -2,44 +2,44 @@ import React, { useState, useEffect } from 'react'
 
 import Moviecon from './Moviecontext';
 import axios from 'axios';
-import response from '../data.json'
+// import response from '../data.json'
 
 export default function Moviestate(props) {
 
-    // const fetchfunction = async () => {
-    //     const options = {
-    //         method: 'GET',
-    //         url: 'https://imdb-top-100-movies.p.rapidapi.com/series',
-    //         headers: {
-    //             'X-RapidAPI-Key': '3e7305e481msh16d5298bb37f8e5p1e53cdjsn661d86249e66',
-    //             'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
-    //         }
-    //     }
+    const fetchfunction = async () => {
+        const options = {
+            method: 'GET',
+            url: 'https://imdb-top-100-movies.p.rapidapi.com/series',
+            headers: {
+                'X-RapidAPI-Key': '3e7305e481msh16d5298bb37f8e5p1e53cdjsn661d86249e66',
+                'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+            }
+        }
 
-    //     try {
-    //         const obj = await axios.request(options);
-    //         console.log(obj.data)
+        try {
+            const obj = await axios.request(options);
+            console.log(obj.data)
 
-    //         setresponse(obj.data.slice(0, 10))
+            setresponse(obj.data.slice(0, 10))
 
 
-    //         //  console.log(response);
-    //         //   var arr1=[];
-    //         //   response.forEach((element)=>{
-    //         //       if(element.genre in arr1){
-    //         //       }
-    //         //       else{
-    //         //         arr1.push(element.genre)
-    //         //       }
-    //         //   })
-    //         // console.log(arr1)
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // }
-    // useEffect(() => {
-    //     fetchfunction();
-    // }, []);
+            //  console.log(response);
+            //   var arr1=[];
+            //   response.forEach((element)=>{
+            //       if(element.genre in arr1){
+            //       }
+            //       else{
+            //         arr1.push(element.genre)
+            //       }
+            //   })
+            // console.log(arr1)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        fetchfunction();
+    }, []);
 
     const host = "https://netflixcopy-eight.vercel.app"
 
